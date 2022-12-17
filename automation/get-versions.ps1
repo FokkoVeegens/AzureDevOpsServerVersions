154c1a803b0d2a46533ed1aaa4cc7d7fdbfc3503
Fokko <24793348+FokkoVeegens@users.noreply.github.com>
2022-12-17 18:41:49 +0000

    Additional functionality for non aka.ms links

--- automation/get-versions.ps1
+++ automation/get-versions.ps1
@@ -1,18 +1,29 @@
 #https://go.microsoft.com/fwlink/?LinkId=2200892
 #https://aka.ms/devops2020.1.2patch4
+#https://devblogs.microsoft.com/devops/now-available-azure-devops-server-2022-rtw/
 
 $ErrorActionPreference = 'Break'
+$regexRemoveHTML = '<[^>]+>'
 
-$response = Invoke-WebRequest -uri "https://devblogs.microsoft.com/devops/december-patches-for-azure-devops-server-2/"
-$downloadLinks = $response.Links | Where-Object { $_.href -like "https://aka.ms*" }
+function ConvertTo-Title($outerHTML) {
+    return $outerHTML -replace '<[^>]+>',''
+}
+
+$response = Invoke-WebRequest -uri "https://devblogs.microsoft.com/devops/now-available-azure-devops-server-2022-rtw/"
+$downloadLinks = $response.Links | `
+    Where-Object { ($_.href -like "https://aka.ms/*" -or `
+                    $_.href -like "https://go.microsoft.com/*") -and `
+                    (ConvertTo-Title -outerHTML $_.OuterHTML) -like "*Azure DevOps*" -and `
+                    (ConvertTo-Title -outerHTML $_.OuterHTML) -notlike "*ISO*"  }
 foreach ($downloadLink in $downloadLinks) {
-    $linkTitle = $downloadLink.OuterHTML -replace '<[^>]+>',''
+    $linkTitle = ConvertTo-Title -outerHTML $downloadLink.OuterHTML
     Write-Output "Processing $linkTitle"
 
+    $ProgressPreference = 'SilentlyContinue'
     Invoke-WebRequest -uri $downloadLink.href -OutFile "./data/azdo.exe"
+    $ProgressPreference = 'Continue'
     $azDoVersionInfo = (Get-Item "./data/azdo.exe").VersionInfo
     if ($azDoVersionInfo | Get-Member | Where-Object { $_.Name -eq "ProductVersion" }) {
-        Write-Output "File found"
         $azDoVersion = $azDoVersionInfo.ProductVersion
         Write-Output "$linkTitle has version $azDoVersion"
     }
