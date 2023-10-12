dc447957ab06a773d38f1882a0a5858a33a7ae1a
Fokko Veegens <24793348+FokkoVeegens@users.noreply.github.com>
2023-10-12 18:38:57 +0000

    October 2023 patches and automation

--- automation/get-versions.ps1
+++ automation/get-versions.ps1
@@ -4,30 +4,35 @@
 
 $ErrorActionPreference = 'Break'
 $regexRemoveHTML = '<[^>]+>'
+$dataPath = "$($PSScriptRoot)/data"
+$downloadFilePath = "$dataPath/azdo.exe"
 
 function ConvertTo-Title($outerHTML) {
     return $outerHTML -replace '<[^>]+>',''
 }
 
-$response = Invoke-WebRequest -uri "https://devblogs.microsoft.com/devops/now-available-azure-devops-server-2022-rtw/"
+if (!(Test-Path -Path $dataPath -PathType Container)) {
+    New-Item -Path $dataPath -ItemType Directory
+}
+
+$response = Invoke-WebRequest -uri "https://devblogs.microsoft.com/devops/october-patches-for-azure-devops-server-3/"
 $downloadLinks = $response.Links | `
-    Where-Object { ($_.href -like "https://aka.ms/*" -or `
-                    $_.href -like "https://go.microsoft.com/*") -and `
+    Where-Object { $_.href -like "https://aka.ms/*" -and `
                     (ConvertTo-Title -outerHTML $_.OuterHTML) -like "*Azure DevOps*" -and `
-                    (ConvertTo-Title -outerHTML $_.OuterHTML) -notlike "*ISO*"  }
+                    (ConvertTo-Title -outerHTML $_.OuterHTML) -notlike "*ISO*"  } | Group-Object -Property href | ForEach-Object { $_.Group[0] }
 foreach ($downloadLink in $downloadLinks) {
     $linkTitle = ConvertTo-Title -outerHTML $downloadLink.OuterHTML
-    Write-Output "Processing $linkTitle"
+    Write-Output -NoNewline "Processing $linkTitle"
 
     $ProgressPreference = 'SilentlyContinue'
-    Invoke-WebRequest -uri $downloadLink.href -OutFile "./data/azdo.exe"
+    Invoke-WebRequest -uri $downloadLink.href -OutFile $downloadFilePath
     $ProgressPreference = 'Continue'
-    $azDoVersionInfo = (Get-Item "./data/azdo.exe").VersionInfo
-    if ($azDoVersionInfo | Get-Member | Where-Object { $_.Name -eq "ProductVersion" }) {
-        $azDoVersion = $azDoVersionInfo.ProductVersion
-        Write-Output "$linkTitle has version $azDoVersion"
-    }
-    else {
-        Write-Output "No file found in link"
+    $FindResults = Select-String -Path $downloadFilePath -Pattern "P\u0000r\u0000o\u0000d\u0000u\u0000c\u0000t\u0000V\u0000e\u0000r\u0000s\u0000i\u0000o\u0000n\u0000\u0000\u0000.*?(\u0000\u0000\u0000\u0000\u0000D)"
+    if ($FindResults.Matches.length -gt 0) {
+        foreach ($match in $FindResults.Matches) {
+            $cleanedMatch = $match -replace '\u0000'
+            $versionNumber = ($cleanedMatch -replace 'ProductVersion') -replace 'D'
+            Write-Host "Version: $versionNumber"
+        }
     }
 }
