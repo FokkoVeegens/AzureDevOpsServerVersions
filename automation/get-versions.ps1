b2a84bf2b4ae7b2224f86081726b4b6288b2a506
Fokko Veegens <24793348+FokkoVeegens@users.noreply.github.com>
2024-01-29 16:56:00 +0100

    Integrated markdown generation

--- automation/get-versions.ps1
+++ automation/get-versions.ps1
@@ -2,37 +2,73 @@
 #https://aka.ms/devops2020.1.2patch4
 #https://devblogs.microsoft.com/devops/now-available-azure-devops-server-2022-rtw/
 
+# Params
+# body.primaryLink
+$blogUrl = "https://devblogs.microsoft.com/devops/now-available-azure-devops-server-2020-1-rtw/"
+# body.publishDate
+$publishDateString = "2024-01-26 18:26:53Z"
+
+
+$publishDate = [datetime]$publishDateString
+$publishDateFormattedString = $publishDate.ToString("dd-MMM-yyyy")
+
 $ErrorActionPreference = 'Break'
-$regexRemoveHTML = '<[^>]+>'
+$Global_RegexRemoveHTML = '<[^>]+>'
 $dataPath = "$($PSScriptRoot)/data"
 $downloadFilePath = "$dataPath/azdo.exe"
 
 function ConvertTo-Title($outerHTML) {
-    return $outerHTML -replace '<[^>]+>',''
+    return $outerHTML -replace $Global_RegexRemoveHTML,''
 }
 
 if (!(Test-Path -Path $dataPath -PathType Container)) {
-    New-Item -Path $dataPath -ItemType Directory
+    $null = New-Item -Path $dataPath -ItemType Directory
+}
+else {
+    Remove-Item -Path $dataPath/* -Recurse -Force
 }
 
-$response = Invoke-WebRequest -uri "https://devblogs.microsoft.com/devops/october-patches-for-azure-devops-server-3/"
+$response = Invoke-WebRequest -uri $blogUrl
 $downloadLinks = $response.Links | `
-    Where-Object { $_.href -like "https://aka.ms/*" -and `
-                    (ConvertTo-Title -outerHTML $_.OuterHTML) -like "*Azure DevOps*" -and `
-                    (ConvertTo-Title -outerHTML $_.OuterHTML) -notlike "*ISO*"  } | Group-Object -Property href | ForEach-Object { $_.Group[0] }
-foreach ($downloadLink in $downloadLinks) {
-    $linkTitle = ConvertTo-Title -outerHTML $downloadLink.OuterHTML
-    Write-Output -NoNewline "Processing $linkTitle"
+    Where-Object { (`
+                        $_.href -like "https://aka.ms/*" -and `
+                        (ConvertTo-Title -outerHTML $_.OuterHTML) -like "*Azure DevOps*" -and `
+                        (ConvertTo-Title -outerHTML $_.OuterHTML) -notlike "*ISO*"`
+                    ) -or `
+                    (`
+                        $_.href -like "https://go.microsoft.com/*" -and `
+                        (ConvertTo-Title -outerHTML $_.OuterHTML) -like "*Web Install*" `
+                    )  } | Group-Object -Property href | ForEach-Object { $_.Group[0] }
 
+$apiVersions = Get-Content -Path "$($PSScriptRoot)/api-versions.json" | ConvertFrom-Json
+$apiVersions | ForEach-Object { $_.ProductVersion = [System.Version]$_.ProductVersion }
+
+$markDown = ""
+foreach ($downloadLink in $downloadLinks) {
+    $linkTitle = (ConvertTo-Title -outerHTML $downloadLink.OuterHTML) -replace "Azure DevOps Server "
+    Write-Output "Processing $linkTitle"
     $ProgressPreference = 'SilentlyContinue'
     Invoke-WebRequest -uri $downloadLink.href -OutFile $downloadFilePath
     $ProgressPreference = 'Continue'
     $FindResults = Select-String -Path $downloadFilePath -Pattern "P\u0000r\u0000o\u0000d\u0000u\u0000c\u0000t\u0000V\u0000e\u0000r\u0000s\u0000i\u0000o\u0000n\u0000\u0000\u0000.*?(\u0000\u0000\u0000\u0000\u0000D)"
     if ($FindResults.Matches.length -gt 0) {
+        if ($FindResults.Matches.length -gt 1) {
+            Write-Warning "Multiple matches found for $linkTitle"
+        }
         foreach ($match in $FindResults.Matches) {
             $cleanedMatch = $match -replace '\u0000'
             $versionNumber = ($cleanedMatch -replace 'ProductVersion') -replace 'D'
+
+            $apiVersionEntry = $apiVersions | Where-Object { $_.ProductVersion -le $versionNumber } | Sort-Object -Descending | Select-Object -First 1
+            if ($apiVersionEntry) {
+                $markDown += "||$($linkTitle)|$($versionNumber)|$($apiVersionEntry.ApiVersion)|$($publishDateFormattedString)|$($blogUrl)|`n"
+            }
+            else {
+                $markDown += "||$($linkTitle)|$($versionNumber)||$($publishDateFormattedString)|$($blogUrl)|`n"
+            }
             Write-Host "Version: $versionNumber"
+            break
         }
     }
 }
+Write-Host $markDown
