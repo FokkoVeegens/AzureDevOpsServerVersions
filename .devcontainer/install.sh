d0c60cb99a38c1f4aa9123a915777dbde82becee
Fokko Veegens <24793348+FokkoVeegens@users.noreply.github.com>
2025-03-12 19:41:30 +0000

    Make pwsh work again and fix for latest blog

---
+++ .devcontainer/install.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+###################################
+# Prerequisites
+
+# Update the list of packages
+sudo apt-get update
+
+# Install pre-requisite packages.
+sudo apt-get install -y wget
+
+# Download the PowerShell package file
+wget https://github.com/PowerShell/PowerShell/releases/download/v7.5.0/powershell_7.5.0-1.deb_amd64.deb
+
+###################################
+# Install the PowerShell package
+sudo dpkg -i powershell_7.5.0-1.deb_amd64.deb
+
+# Resolve missing dependencies and finish the install (if necessary)
+sudo apt-get install -f
+
+# Delete the downloaded package file
+rm powershell_7.5.0-1.deb_amd64.deb
+
+# Start PowerShell Preview
+pwsh
